a9770b6d1808b7f40387736a7a3bda256c8eb01b
SantiagoE <s.estupinan@uniandes.edu.co>
2019-11-03 21:56:41 -0500

    s

---
+++ st-EPSAndes/src/main/java/view/EPSAndesview.java
@@ -0,0 +1,113 @@
+package view;
+
+public class EPSAndesview {
+
+
+	public void printMenu() {
+		System.out.println("---------------------Iteracion 1----------------------");
+		System.out.println("---------------------Seleccione usuario----------------------");
+		System.out.println("");
+		System.out.println("1. Administrador");
+		System.out.println("2. Medico");
+		System.out.println("3. Afiliado");
+		System.out.println("4. Recepsionista");
+		System.out.println("5. Gerente");
+		System.out.println("6. Salir");
+		System.out.println("");
+		System.out.println("Digite el número de opción para ejecutar la tarea, luego presione enter: (Ej., 1):");
+
+	}
+	
+	public void printMenuAdministrador() {
+		System.out.println("---------------------Seleccione requerimiento----------------------");
+		System.out.println("");
+		System.out.println("1. MOSTRAR LA CANTIDAD DE SERVICIOS PRESTADOS POR CADA IPS DURANTE UN PERIODO DE TIEMPO Y EN EL AÑO CORRIDO");
+		System.out.println("2. MOSTRAR LOS 20 SERVICIOS MÁS SOLICITADOS.");
+		System.out.println("3. MOSTRAR EL ÍNDICE DE USO DE CADA UNO DE LOS SERVICIOS PROVISTOS");
+		System.out.println("4. MOSTRAR LOS SERVICIOS QUE CUMPLEN CON CIERTA CARACTERÍSTICA");
+		System.out.println("5. MOSTRAR LA UTILIZACIÓN DE SERVICIOS DE EPSANDES POR UN AFILIADO DADO, EN UN RANGO DE FECHAS INDICADO");
+		System.out.println("6. REGISTRAR USUARIOS");
+		System.out.println("7. REGISTRAR IPS");
+		System.out.println("8. REGISTRAR UN SERVICIO DE SALUD PRESTADO POR UNA IPS");
+		System.out.println("9. REGISTRAR CAMPAÑA");
+		System.out.println("10. CANCELAR SERVICIOS DE LA CAMPAÑA");
+		System.out.println("11. DESHABILITAR SERVICIOS DE SALUD");
+		System.out.println("12. REGISTRAR LA REAPERTURA DE SERVICIOS DE SALUD");
+		System.out.println("");
+		System.out.println("Digite el número de opción para ejecutar la tarea, luego presione enter: (Ej., 1):");
+
+	}
+	
+	public void printMenuMedico() {
+		System.out.println("---------------------Seleccione requerimiento----------------------");
+		System.out.println("");
+		System.out.println("1. REGISTRAR UNA ORDEN DE SERVICIO DE SALUD PARA UN AFILIADO POR PARTE DE UN MÉDICO");
+		System.out.println("");
+		System.out.println("Digite el número de opción para ejecutar la tarea, luego presione enter: (Ej., 1):");
+
+	}
+	
+	public void printMenuAfiliado() {
+		System.out.println("---------------------Seleccione requerimiento----------------------");
+		System.out.println("");
+		System.out.println("1. REALIZAR LA RESERVA DE UN SERVICIO DE SALUD POR PARTE DE UN AFILIADO");
+		System.out.println("");
+		System.out.println("Digite el número de opción para ejecutar la tarea, luego presione enter: (Ej., 1):");
+
+	}
+	
+	public void printMenuRecepcionista() {
+		System.out.println("---------------------Seleccione requerimiento----------------------");
+		System.out.println("");
+		System.out.println("1. REGISTRAR LA PRESTACIÓN DE UN SERVICIO DE SALUD A UN AFILIADO POR PARTE DE UNA IPS");
+		System.out.println("");
+		System.out.println("Digite el número de opción para ejecutar la tarea, luego presione enter: (Ej., 1):");
+
+	}
+	
+	
+	public void printMenuGerente() {
+		System.out.println("---------------------Seleccione requerimiento----------------------");
+		System.out.println("");
+		System.out.println("1. MOSTRAR LA CANTIDAD DE SERVICIOS PRESTADOS POR CADA IPS DURANTE UN PERIODO DE TIEMPO Y EN EL AÑO CORRIDO");
+		System.out.println("2. MOSTRAR LOS 20 SERVICIOS MÁS SOLICITADOS.");
+		System.out.println("3. MOSTRAR EL ÍNDICE DE USO DE CADA UNO DE LOS SERVICIOS PROVISTOS");
+		System.out.println("4. MOSTRAR LOS SERVICIOS QUE CUMPLEN CON CIERTA CARACTERÍSTICA");
+		System.out.println("5. MOSTRAR LA UTILIZACIÓN DE SERVICIOS DE EPSANDES POR UN AFILIADO DADO, EN UN RANGO DE FECHAS INDICADO");
+		System.out.println("6. REGISTRAR UNA ORDEN DE SERVICIO DE SALUD PARA UN AFILIADO POR PARTE DE UN MÉDICO");
+		System.out.println("7. REALIZAR LA RESERVA DE UN SERVICIO DE SALUD POR PARTE DE UN AFILIADO");
+		System.out.println("8. REGISTRAR LA PRESTACIÓN DE UN SERVICIO DE SALUD A UN AFILIADO POR PARTE DE UNA IPS");
+		System.out.println("9. REGISTRAR CAMPAÑA");
+		System.out.println("10. CANCELAR SERVICIOS DE LA CAMPAÑA");
+		System.out.println("11. DESHABILITAR SERVICIOS DE SALUD");
+		System.out.println("12. REGISTRAR LA REAPERTURA DE SERVICIOS DE SALUD");
+		System.out.println("13. ANALIZAR LA OPERACIÓN DE EPSANDES");
+		System.out.println("14. ENCONTRAR LOS AFILIADOS EXIGENTES");
+		System.out.println("15. ENCONTRAR LOS SERVICIOS QUE NO TIENEN MUCHA DEMANDA");
+		System.out.println("16. REGISTRAR USUARIOS");
+		System.out.println("17. REGISTRAR IPS");
+		System.out.println("18. REGISTRAR UN SERVICIO DE SALUD PRESTADO POR UNA IPS");
+		System.out.println("");
+		System.out.println("Digite el número de opción para ejecutar la tarea, luego presione enter: (Ej., 1):");
+
+	}
+	
+	public void printMenuRegistrarUsuario() {
+		System.out.println("---------------------Seleccione requerimiento----------------------");
+		System.out.println("");
+		System.out.println("1. Registrar un gerente");
+		System.out.println("2. Registrar un administrador");
+		System.out.println("3. Registrar un recepcionista");
+		System.out.println("4. Registrar un medico");
+		System.out.println("5. Registrar un afiliado");
+		System.out.println("");
+		System.out.println("Digite el número de opción para ejecutar la tarea, luego presione enter: (Ej., 1):");
+
+	}
+	
+	
+
+	public void printMessage(String mensaje) {
+		System.out.println(mensaje);
+	}
+}
